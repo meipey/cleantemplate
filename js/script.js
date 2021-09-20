82e35456fd1214123cd21b19d6cd6e7280e441e5
meipey <82648623+meipey@users.noreply.github.com>
2021-09-20 12:11:42 +0200

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":980},"pages":[{"id":"page-homepage","displayName":"homepage","link":{"linkType":"LinkTypePage","href":"#!page-homepage"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"Contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":980},"pages":[{"id":"page-homepage","displayName":"homepage","link":{"linkType":"LinkTypePage","href":"#!page-homepage"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
